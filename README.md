97c8faf33e691b194104fee75641389de2325c07
Harsh-Vaishani <harshvaishnani8@gmail.com>
2024-01-30 11:21:30 +0530

    pointer

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # pointer-
+![Alt text](<pointer 2.png>) ![Alt text](<pointer 3.png>) ![Alt text](pointer-1.png)
---
+++ index.c
@@ -0,0 +1,53 @@
+
+// --- --- --- --- --  -- - - - - - - - - - - -- - QUESTION-1
+
+// #include <stdio.h>
+// void fun(int x, int y){
+//    printf("Before Swipe => %d ",x);
+//    printf("%d\n",y);
+//     int temp;
+//     temp=x;
+//     x=y;
+//     y=temp;
+//     printf("After Swipe => %d ",x);
+//     printf("%d\n",y);
+// }
+// int main()
+// {
+//    int x=5;
+//    int y=3;
+//    fun(x,y);
+//    return 0;
+// }
+
+
+    // --- --- --- --- --  -- - - - - - - - - - - -- -  QUESTION-2
+
+
+// #include <stdio.h>
+// int main(){
+// int array[]={7,8,9,1,2};
+// int *p=&array;
+// printf("%u %d\n",p,*p);
+// printf("%u %d\n",p+1,*(p+1));
+// printf("%u %d\n",p+2,*(p+2));
+// printf("%u %d\n",p+3,*(p+3));
+// printf("%u %d\n",p+4,*(p+4));
+//    return 0;
+// }
+
+
+
+ // --- --- --- --- --  -- - - - - - - - - - - -- -  QUESTION-3
+
+
+
+// #include <stdio.h>
+// int main(){
+//      int a=20;
+//     int *p=&a;
+//     int **p2;
+//     p2=&p;
+//     printf("%u => %d ", p2, **p2);
+//     return 0;
+// }
